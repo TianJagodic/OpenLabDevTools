d98c2aab4d4e71c7adfbf33b5ea8d0b0918d52f4
Tian <tian.jago@gmail.com>
2019-07-07 11:42:48 +0200

    Google Login not working

--- Login/Login.js
+++ Login/Login.js
@@ -1,8 +1,11 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+firebase.auth().languageCode = 'pt';
 
 document.getElementById("LoginButtonClick").onclick = OnLogin;
 
 function OnLogin() {
+  console.log("We have hit the login button");
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;

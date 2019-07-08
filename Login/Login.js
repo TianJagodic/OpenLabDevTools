295f1995ad9ce12250a3c242cee6226cfcfc5223
Tian <tian.jago@gmail.com>
2019-07-08 20:24:43 +0200

    Working Email Register

--- Login/Login.js
+++ Login/Login.js
@@ -2,11 +2,12 @@ var provider = new firebase.auth.GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 firebase.auth().languageCode = 'pt';
 
-document.getElementById("LoginButtonClick").onclick = OnLogin;
+document.getElementById("LoginWithGoogle").onclick = OnLogin;
 
-function OnLogin() {
+async function OnLogin() {
   console.log("We have hit the login button");
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup(provider).then(async function(result) {
+    console.log("Something happend");
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
@@ -22,4 +23,85 @@ function OnLogin() {
     var credential = error.credential;
     // ...
   });
+}
+
+//-------------------------Register && login with email part of this damed script
+
+// signup
+const signupForm = document.querySelector('#register-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+
+    // close the signup modal & reset form
+    RegisterModal.style.display = "none";
+    signupForm.reset();
+
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    document.getElementById("RegisterErrorText").innerText = errorMessage;
+  });
+});
+
+
+
+//---------------------------Modal stuff and things
+
+// Get the modal
+var Loginmodal = document.getElementById("LoginModal");
+var RegisterModal = document.getElementById("RegisterModal");
+var LoginEmailModal = document.getElementById("LoginEmailModal");
+
+// Get the button that opens the modal
+var Loginbtn = document.getElementById("LoginButtonClick");
+var RegisterBtn = document.getElementById("RegisterButtonClick");
+var LoginEmailBtn = document.getElementById("LoginEmailClick");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+Loginbtn.onclick = function() {
+  Loginmodal.style.display = "block";
+};
+
+LoginEmailBtn.onclick = function(){
+  LoginEmailModal.style.display = "block";
+};
+
+RegisterBtn.onclick = function(){
+  RegisterModal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  Loginmodal.style.display = "none";
+  RegisterModal.style.display = "none";
+  LoginEmailModal.style.display = "none";
+};
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == Loginmodal || event.target == RegisterModal || event.target == LoginEmailModal) {
+    Loginmodal.style.display = "none";
+    RegisterModal.style.display = "none";
+    LoginEmailModal.style.display = "none";
+    L
+  }
 }

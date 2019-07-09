3f019f36fc816f811fb6a40df00c43ff26091388
Tian <tian.jago@gmail.com>
2019-07-09 18:48:25 +0200

    Getting tasks from dataBase

--- Apps/Tasks/Tasks.js
+++ Apps/Tasks/Tasks.js
@@ -1,10 +1,28 @@
-// listen for auth status changes
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    console.log('user logged in: ', user);
-    HideLogin();
-  } else {
-    console.log('user logged out');
-    ShowLogin();
-  }
+const TabList = document.querySelector("#AllTasks");
+
+db.collection('Tasks').get().then((snapshot) => {
+  console.log(snapshot.docs);
+  snapshot.docs.forEach(doc => {
+    console.log(doc.data().title);
+    ShowTab(doc);
+  });
 });
+
+function ShowTab(doc){
+  console.log("We are creating all the tabs");
+  //Create all the contents
+  let Tab = document.createElement('div');
+  let TabName = document.createElement('span');
+
+
+
+  //Add Classes to the parts
+  Tab.className = "Task";
+  Tab.alt = doc.data().title;
+  TabName.className = "TaskTitle";
+
+  TabName.textContent = doc.data().title;
+
+  Tab.appendChild(TabName);
+  TabList.appendChild(Tab);
+}
